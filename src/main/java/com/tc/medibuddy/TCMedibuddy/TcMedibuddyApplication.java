9823bd8830853d0e9f3501a93d7fc166de208007
akdarun <arunkumar94@deloitte.com>
2022-11-04 10:12:54 +0530

    initial push for project structure

---
+++ src/main/java/com/tc/medibuddy/TCMedibuddy/TcMedibuddyApplication.java
@@ -0,0 +1,13 @@
+package com.tc.medibuddy.TCMedibuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcMedibuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcMedibuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tc/medibuddy/TCMedibuddy/TcMedibuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.medibuddy.TCMedibuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcMedibuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
